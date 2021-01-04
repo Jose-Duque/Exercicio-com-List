0858f3c353fe4ba87c409691c9089b83e801e4af
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-01-04 14:49:25 -0300

    get and set

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,25 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employees;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employees> employees = new ArrayList<Employees>();
+		
+		System.out.print("How many employees will be registered");
+		int n = sc.nextInt();
+		
+		sc.close();
 	}
 
 }
